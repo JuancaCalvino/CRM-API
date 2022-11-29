808352d88ba7dbf93100078362efc4e7afa75fdf
JuancaCalvino <juancacg99@gmail.com>
2022-11-29 23:27:37 +0100

    First Test not pass

--- Backend Springboot/CRM/src/test/java/com/CRM/CRM/Service/RegisterServiceTest.java
+++ Backend Springboot/CRM/src/test/java/com/CRM/CRM/Service/RegisterServiceTest.java
@@ -1,14 +1,35 @@
 package com.CRM.CRM.Service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collection;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.CRM.CRM.Models.User;
+import com.CRM.CRM.Services.UserService;
 
 public class RegisterServiceTest {
+	
+	@Autowired
+    private UserService userService;
 	
 	@Test
 	void userNameFieldShouldNotBeEmpty() {
 		
+		User user1 = new User();
+		user1.setName(null);
+		user1.setEmail("solera@solera.com");
+		user1.setAddress("C/Solera");
+		user1.setPhone(666666666);
+		user1.setPassword("contrasenaprueba");
+		
+		userService.saveUser(user1);
+		
+		Collection<User> users = this.userService.findUserById(user1.getUser_ID());
+		int found = users.size();
+		assertThat(users.size()).isEqualTo(found+1);
 	}
 	
 	@Test
@@ -45,4 +66,7 @@ public class RegisterServiceTest {
 	void phoneFieldShouldBePhone() {
 		
 	}
+	
+	@Test
+	void 
 }
